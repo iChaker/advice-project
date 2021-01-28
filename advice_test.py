a02a23fe1eeea4dbac58d3956098b5384ef0d01d
Iyadh Chaker <chaker.iyadh@gmail.com>
2021-01-28 19:22:45 +0100

    fixed test

--- advice_test.py
+++ advice_test.py
@@ -2,9 +2,7 @@ import unittest
 import advice as tested_app
 import json
 
-good_advice_list = tested_app.good_advice_list
 
-bad_advice_list = tested_app.bad_advice_list
 
 
 class FlaskAppTests(unittest.TestCase):
@@ -14,12 +12,15 @@ class FlaskAppTests(unittest.TestCase):
         self.app = tested_app.app.test_client()
 
     def test_good_advice(self):
-        print("hahaha")
         r = self.app.get('/goodadvice')
         
-        self.assertIn(r.data,good_advice_list )
+        self.assertIn(r.data.decode('ascii'),tested_app.good_advice_list )
 
     def test_bad_advice(self):
         r = self.app.get('/badadvice')
-        print("AWAHWAHWHAH")
-        self.assertEqual(r.data, bad_advice_list)
+        self.assertIn(r.data.decode('ascii'), tested_app.bad_advice_list)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
